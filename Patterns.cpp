dda34a4ad958cd3833d7ca76909e1f4d89a299f2
saccharok <90149992+saccharok@users.noreply.github.com>
2023-05-08 18:40:32 +0700

    Add files via upload

--- Patterns.cpp
+++ Patterns.cpp
@@ -1,62 +1,54 @@
 ﻿// Patterns - Земская ПИ-02
-
+#define _CRT_SECURE_NO_WARNINGS
 //lab5-6
-#include "IDocs.h"
-#include "Pack.h"
-#include "Box.h"
-
+#include "Bridge.h"
+#include "InformationExpert.h"
+#include "Facade.h"
 #include <iostream>
 #include <string.h>
 using namespace std;
 
 int main()
 {
-	cout << "3 - Adapter" << endl;
-	cout << "4 - Decorator" << endl;
-	cout << "5 - Composite" << endl;
-	//cout << "6 - Iterator" << endl;
+	cout << "6 - Bridge" << endl;
+	cout << "7 - Information Expert" << endl;
+	cout << "8 - Facade" << endl;
     int type = 0;
     cin >> type;
     switch (type)
     {
-		case(3):
+		case(6):
 		{
-			cout << "Order#1 - 102030" << endl;
-			OriginDoc* od = new OriginDoc();
-			string n = "102030";
-			n = od->readDoc(n);
-			cout << "Order#2 - 102031" << endl;
-			n = "102031";
-			IDocs* conv = new Adapter();
-			conv->getDoc(n);
+			IDelivery* idel = new Purchase();
+			Delivery* del = new Delivery(idel);
+			del->setSettings();
+			del->showSettings();
+			cout << endl;
+			del->customizedType("Chip delivery");
+			del->showSettings();
+			cout << endl;
+			del->customizedKind("Courier delivery");
+			del->showSettings();
+			cout << endl;
 			break;
 		}
 
-		case(4):
+		case(7):
 		{
-			Packing pack;
-			cout << pack.getInfo() << "Cost: " << pack.getCost();
-			Delicate del(&pack);
-			cout << del.getInfo() << "Cost: " << del.getCost();
-			NotWet nw(&del);
-			NotFlip nf(&nw);
-			NotOpen no(&nf);
-			NotUseHook nuh(&no);
-			cout << nuh.getInfo() << "Cost: " << nuh.getCost();
+			OrderCost* Order = new OrderCost();
+			string name[] = { "Lemonade" , "Coffee" , "Sweets" };
+			int count[] = { 5, 6, 3 };
+			int price = 0;
+			price = Order->getCost(3, name, count);
+			cout << endl << "Total order price: " << price;
 			break;
 		}
 
-		case(5):
+		case(8):
 		{
-			Composite box;
-			box.about();
-			IBox* knife = new Product("knife");
-			knife->about();
-			box.addProd(knife);
-			IBox* fork = new Product("fork");
-			fork->about();
-			box.addProd(fork);
-			box.delProd(knife);
+			Facade* fac = new Facade();
+			fac->Goods();
+			fac->Transfer();
 			break;
 		}
     }
