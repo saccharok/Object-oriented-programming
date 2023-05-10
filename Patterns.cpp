0adc58302dfabb89639a4b6526617caf5baa7a40
saccharok <90149992+saccharok@users.noreply.github.com>
2023-05-11 04:35:15 +0700

    main

--- Patterns.cpp
+++ Patterns.cpp
@@ -1,54 +1,41 @@
 ﻿// Patterns - Земская ПИ-02
 #define _CRT_SECURE_NO_WARNINGS
-//lab5-6
-#include "Bridge.h"
-#include "InformationExpert.h"
-#include "Facade.h"
 #include <iostream>
 #include <string.h>
 using namespace std;
-
+#include "FactoryMethod.h"
+#include "AbstractFactory.h"
 int main()
 {
-	cout << "6 - Bridge" << endl;
-	cout << "7 - Information Expert" << endl;
-	cout << "8 - Facade" << endl;
+	cout << "9 - Factory method" << endl;
+	cout << "10 - Abstract factory" << endl;
     int type = 0;
     cin >> type;
     switch (type)
     {
-		case(6):
+		case(9):
 		{
-			IDelivery* idel = new Purchase();
-			Delivery* del = new Delivery(idel);
-			del->setSettings();
-			del->showSettings();
+			DeliveryType* del1 = new Express();
+			Transport* t1 = del1->operation();
 			cout << endl;
-			del->customizedType("Chip delivery");
-			del->showSettings();
+			DeliveryType* del2 = new Standard();
+			Transport* t2 = del2->operation();
 			cout << endl;
-			del->customizedKind("Courier delivery");
-			del->showSettings();
+			DeliveryType* del3 = new Cheap();
+			Transport* t3 = del3->operation();
 			cout << endl;
 			break;
 		}
 
-		case(7):
+		case(10):
 		{
-			OrderCost* Order = new OrderCost();
-			string name[] = { "Lemonade" , "Coffee" , "Sweets" };
-			int count[] = { 5, 6, 3 };
-			int price = 0;
-			price = Order->getCost(3, name, count);
-			cout << endl << "Total order price: " << price;
-			break;
-		}
-
-		case(8):
-		{
-			Facade* fac = new Facade();
-			fac->Goods();
-			fac->Transfer();
+			cout << endl << "Order route:" << endl;
+			Factory* f1 = new Storage();
+			Show* s1 = new Show(f1);
+			Factory* f2 = new SortingCenter();
+			Show* s2 = new Show(f2);
+			Factory* f3 = new PickupPoint();
+			Show* s3 = new Show(f3);
 			break;
 		}
     }
